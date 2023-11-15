4407c8370e7da1ca3407905e22e6d4ff516cd34b
LiuSuny <119320935+LiuSuny@users.noreply.github.com>
2023-11-15 12:01:56 +0300

    Added more changes like using C# code inside html and working with bootstrap and bootswatch to make our project unique

--- ASP_WebApi/Controllers/CategoryController.cs
+++ ASP_WebApi/Controllers/CategoryController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASP_WebApi.Data;
+using ASP_WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ASP_WebApi.Controllers
 {
     public class CategoryController : Controller
     {
+        //On a normal we can initiate applicationDbConnection db = new applicationDbConnection() if at all we are not working with .Net core and we are using legacy.net application but in these case we would not
+        private readonly ApplicationDBConnection _db; //whatever we get through our constructor will be assigned to our local variable
+        public CategoryController(ApplicationDBConnection db)
+        {
+            _db = db;
+        }
+        //the above constructor and local variable will allow to use our IActionResult method inside any other action method
         public IActionResult Index()
         {
-            return View();
+            //Next we want to retrieve our Category with table and class and we don't need sql for that just entityframework is okay
+
+          List<Category> objCategory =  _db.Category.ToList(); //meaning we are retrieving our data under DbSet  ApplicationDBConnection class
+            return View(objCategory); //passing the category view to controller
         }
     }
 }
--- ASP_WebApi/Program.cs
+++ ASP_WebApi/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-//next we want to tell our builder that we need to use entityframework.core
+//next we want to tell our builder that we need to use entityframework.core(sort of dependence injection )
 builder.Services.AddDbContext<ApplicationDBConnection>(option => 
 option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); //using option to tell our connection that we want to use sql server note: we purposely added defaultConnect1 on jason appsetting is different
 var app = builder.Build();
