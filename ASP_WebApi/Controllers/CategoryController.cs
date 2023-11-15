10bea20b86df485df0025727382db52569ba165a
LiuSuny <119320935+LiuSuny@users.noreply.github.com>
2023-11-15 21:54:12 +0300

    Server and client side Validation DONE add a customized validation as well

--- ASP_WebApi/Controllers/CategoryController.cs
+++ ASP_WebApi/Controllers/CategoryController.cs
@@ -20,5 +20,28 @@ namespace ASP_WebApi.Controllers
           List<Category> objCategory =  _db.Category.ToList(); //meaning we are retrieving our data under DbSet  ApplicationDBConnection class
             return View(objCategory); //passing the category view to controller
         }
+
+        public IActionResult Create() //this method help with creation of new page or category once new category is cclick on our site
+        {
+          
+            return View(); 
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj) //this method help with Post of our create FORM page inside create.cshtml section 
+        {
+            if (obj.Name == obj.DisplayOrders.ToString())
+            {
+                //AddModelError(key, string) key being our name and normal text display option
+                ModelState.AddModelError("Name", "The DisplayOrders cannot exactly match the Name");
+            }
+            if (ModelState.IsValid) //this basically check if the modestate(obj) isvalid then it will go to Category
+            {
+                _db.Category.Add(obj); //this line here is telling entity framework to add any input data we inputed to this category table on database but to save it we use _db.savechanges();
+                _db.SaveChanges(); //this line tells database to create and save on our database
+                return RedirectToAction("Index"); //redirectionToAction will help us redirect to the index or which action we need to
+            }
+            return View();
+        }
     }
 }
--- ASP_WebApi/Models/Category.cs
+++ ASP_WebApi/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ASP_WebApi.Models
 {
@@ -6,8 +7,12 @@ namespace ASP_WebApi.Models
     {
         [Key]
         public int Id { get; set; } 
-        [Required] //these means that Name is required(must which is not null) 
+        [Required] //these means that Name is required(must which is not null
+        [MaxLength(30)] /////////max 30 character
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+        [DisplayName("Display Order")] //this data annotation which help with client side validation
+        [Range(1,100, ErrorMessage ="Display order must be between 1-100")] /////////Here are adding validation min 1, max 100
         public  int   DisplayOrders { get; set; }
 
     }
