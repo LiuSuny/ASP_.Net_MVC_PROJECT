a270fbfdfd4a276e615d7ca85bdf2d7e758d4164
LiuSuny <119320935+LiuSuny@users.noreply.github.com>
2023-11-16 19:23:26 +0300

    Edit, delete and notification DONE

--- ASP_WebApi/Controllers/CategoryController.cs
+++ ASP_WebApi/Controllers/CategoryController.cs
@@ -40,9 +40,77 @@ namespace ASP_WebApi.Controllers
             {
                 _db.Category.Add(obj); //this line here is telling entity framework to add any input data we inputed to this category table on database but to save it we use _db.savechanges();
                 _db.SaveChanges(); //this line tells database to create and save on our database
+                TempData["success"] = "Category created succeessfully";
                 return RedirectToAction("Index"); //redirectionToAction will help us redirect to the index or which action we need to
             }
             return View();
+        }
+
+
+        public IActionResult Edit(int? id) //this method help in editing etc and by adding int? null
+        {
+            if(id== null || id==0)
+            {
+                return NotFound();
+            }
+            //Next we need to retrive our edit from database using several method
+            Category? categoryFromDb = _db.Category.Find(id); //this method only work with primary key
+            //Category? categoryFromDb1 = _db.Category.FirstOrDefault(u=>u.Id==id); //second method this can be modify in several ways 
+            //Category? categoryFromDb2 = _db.Category.Where(u => u.Id == id).FirstOrDefault(); //third method but the second is more preferable
+            if (categoryFromDb ==null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj) //this method help with Post of our create FORM page inside create.cshtml section 
+        {
+           
+            if (ModelState.IsValid) //this basically check if the modestate(obj) isvalid then it will go to Category
+            {
+                _db.Category.Update(obj); //this line here is telling entity framework to update any input data we inputed to this category table on database but to save it we use _db.savechanges();
+                _db.SaveChanges(); //this line tells database to create and save on our database
+                TempData["success"] = "Category updated succeessfully";
+                return RedirectToAction("Index"); //redirectionToAction will help us redirect to the index or which action we need to
+            }
+            return View();
+        }
+
+
+        //DELETE OPTION 
+        public IActionResult Delete(int? id) //this method help in editing etc and by adding int? null
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            //Next we need to retrive our edit from database using several method
+            Category? categoryFromDb = _db.Category.Find(id); //this method only work with primary key
+            //Category? categoryFromDb1 = _db.Category.FirstOrDefault(u=>u.Id==id); //second method this can be modify in several ways 
+            //Category? categoryFromDb2 = _db.Category.Where(u => u.Id == id).FirstOrDefault(); //third method but the second is more preferable
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id) //this method help with Post of our create FORM page inside create.cshtml section 
+        {
+            Category? obj= _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Category.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted succeessfully";
+            return RedirectToAction("Index"); //redirectionToAction will help us redirect to the index or which action we need to
+           
+            //return View(); 
         }
     }
 }
