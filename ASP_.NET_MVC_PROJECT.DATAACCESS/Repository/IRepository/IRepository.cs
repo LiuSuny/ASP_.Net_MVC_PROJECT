36e45d519e8895b98a801b81c99279f9dd8df082
LiuSuny <119320935+LiuSuny@users.noreply.github.com>
2023-11-20 22:32:04 +0300

    Implemented Interface Repository and realize our Repository using classes

---
+++ ASP_.NET_MVC_PROJECT.DATAACCESS/Repository/IRepository/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP_.Net_MVC_PROJECT.DATAACCESS.Repository.IRepository
+{
+    //One thing to take note of when working with generic is that we didn't know what type it will be.
+    public interface IRepository<T> where T :class
+    {
+        //T-GetAlll
+        //Implementing the interface our repository as we using generic collection to implement it 
+
+        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T, bool>> filter);   //we can use GetFirstOrDefault() or GetValue all are the same
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entity);
+        //Note that we already have Update()in our CategoryController reason why it not necessary
+        //Next we need to create new class which will be use to realize our interface
+
+
+    }
+}
---
+++ ASP_.NET_MVC_PROJECT.DATAACCESS/Repository/Repository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using ASP_.Net_MVC_PROJECT.DATAACCESS.Data;
+using ASP_.Net_MVC_PROJECT.DATAACCESS.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP_.Net_MVC_PROJECT.DATAACCESS.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        //Adding dependence injection
+
+        private readonly ApplicationDBConnection _db;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDBConnection db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+            //db.Category = dbSet;
+
+        }
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault(); ////almost the same thing we did in CategoryController categoryFromDb2 = _db.Category.Where(u => u.Id == id).FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            dbSet.RemoveRange(entity);
+        }
+    }
+}
